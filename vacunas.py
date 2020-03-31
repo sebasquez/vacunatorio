5092f2a8f5a7756b7737cb9327b5786b549b9ccf
natisepulvedamora <62262105+natisepulvedamora@users.noreply.github.com>
2020-03-31 01:14:24 -0300

    Se agregaron las funcionalidades de insertar vacuna a paciente

--- vacunas.py
+++ vacunas.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import times
 from flask import Flask, render_template, jsonify, request
 from flaskext.mysql import MySQL
 import pymysql
@@ -20,20 +21,57 @@ mysql.connect_args["cursorclass"] = pymysql.cursors.DictCursor #Transforma las t
 def inicio():
 	#dejar esta pagina como el menú inicial
 	#en la plantilla leo los datos 
-	return render_template("index.html")
-
-
-@app.route('/addVacuna') #Listado de vacunas
-def vacunarPaciente():
 	cursor = mysql.get_db().cursor()
 	cursor.execute("SELECT * FROM paciente") #Muestra los pacientes desde la BD
 	paciente=cursor.fetchall()
-	cursor_vacuna = mysql.get_db().cursor()
+	return render_template("inicio.html",pacientes=paciente)
+
+
+@app.route('/addVacuna',methods=['POST']) #Listado de vacunas
+def addVacuna():
 	
+	
+	#cursor = mysql.get_db().cursor()
+	#cursor.execute("SELECT * FROM paciente") #Muestra los pacientes desde la BD
+	#paciente=cursor.fetchall()
+	rut=request.form['rut']
+	cursor_paciente = mysql.get_db().cursor()
+	sql="SELECT * FROM paciente WHERE RUT = %s"
+	cursor_paciente.execute(sql,rut)
+	paciente=cursor_paciente.fetchall()
+	
+	cursor_vacuna = mysql.get_db().cursor()
 	cursor_vacuna.execute("SELECT * FROM vacuna") #Muestra los pacientes desde la BD
 	vacuna=cursor_vacuna.fetchall()
-
 	return render_template("addVacuna.html",pacientes=paciente,vacunas=vacuna)
+		
+
+
+@app.route('/addDatosVacuna', methods=['POST']) #Listado de vacunas
+def addDatosVacuna():	
+	try:
+		datos=request.form
+		rut=datos['RUT']
+		vacuna=datos['NOMBRE_ENFERMEDAD']
+		fecha=times.now()	
+		cursor_insertar = mysql.get_db().cursor()
+		sql="INSERT INTO PACIENTE_RECIBE_VACUNA(RUT,NOMBRE_ENFERMEDAD,FECHA_VACUNACION) VALUES(%s,%s,%s)"
+		cursor_insertar.execute(sql,(rut,vacuna,fecha))
+		mensaje="Su registró se guardó correctamente"
+		return render_template("addDatosVacuna.html",e = mensaje)
+
+		
+
+	except Exception as e:
+		mensaje2="No fue posible guardar su registro"
+		return render_template("addDatosVacuna.html",e = mensaje2)
+
+	
+	
+
+
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
